56cf53b10ee4c1571b3ce34e9b1d5a2e94a135ca
Yaira V <39809411+Yvv1214@users.noreply.github.com>
2023-09-28 02:08:13 +0000

    added content to sports.js

--- src/front/js/pages/sports.js
+++ src/front/js/pages/sports.js
@@ -1,26 +1,48 @@
 import React from "react";
-
-
+import { useEffect } from "react";
+import { Link } from "react-router-dom";
 
 
 
 
 export const Sports = () => {
+
+    const src = "https://www.youtube.com/embed/3mHmjSosJRg&t=85s";
     
 
 
     return (
 
-        <div>
-            <h1 className="text-center mt-5">Sports you can do</h1>
 
-            <div>
-                <p className="ps-5 pe-5 mt-5">Are you an active soul, constantly seeking new adventures to cure your boredom? Well, guess what?
+        <div>
+            <h1 className="text-center mt-5">Sports you can do &#9977;</h1>
+
+            <div className="d-flex row gx-0">
+                <p className="ps-5 pe-5 mt-5 py-2 col-lg-8" style={{background: 'whitesmoke'}}>Are you an active soul, constantly seeking new adventures to cure your boredom? Well, guess what?
                  The world of sports is your ultimate playground! Brace yourself, because there's a whole universe of thrilling activities out there just waiting for you to discover. 
                  And here's the best part: chances are, you haven't even heard of most of them! So why not dive headfirst into the unknown and give these hidden gems a shot? 
                  Get ready to unleash your inner explorer and embark on an exhilarating journey through uncharted sporting territories. Who knows what incredible talents and passions you might uncover along the way? 
                  So, gear up, my friend, because it's time to break free from the mundane and embrace the extraordinary. Let the games begin!</p>
+            
+
+            <div className="sportsListBtn m-auto justify-content-center d-flex mt-3 col-lg-3"> 
+                <a href='https://www.topendsports.com/sport/list/index.htm' target="_blank" rel="noreferrer">
+                    <button className="btn btn-primary">Show Me the List</button> 
+                </a>
             </div>
+            </div>
+
+            <div className="d-flex justify-content-center mt-3">
+            <iframe
+                width="560"
+                height="315"
+                src={src}
+                title="Youtube Player"
+                frameborder="0"
+                allowFullScreen
+            />
+            </div>
+
         </div>
     )
 }
